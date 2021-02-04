fd17ac85366d35f5904ad425405c65e4b510d43b
boko <40710040+pinoerumo@users.noreply.github.com>
2021-02-04 22:23:26 +0900

    不必要ファイルの削除、ディレクトリの整理

---
+++ RxswiftMvvmSample/Pods/Target Support Files/Pods-RxswiftMvvmSample-RxswiftMvvmSampleUITests/Pods-RxswiftMvvmSample-RxswiftMvvmSampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxswiftMvvmSample_RxswiftMvvmSampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RxswiftMvvmSample_RxswiftMvvmSampleUITests
+@end
---
+++ RxswiftMvvmSample/Pods/Target Support Files/Pods-RxswiftMvvmSampleTests/Pods-RxswiftMvvmSampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxswiftMvvmSampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_RxswiftMvvmSampleTests
+@end
